34114cbb33fbbd7ee9b7194f8355f9305026a299
khangvo <audi11012011@gmail.com>
2025-07-18 10:11:18 +0700

    feat: add feat render

--- js/index.js
+++ js/index.js
@@ -5,14 +5,37 @@ document.addEventListener("DOMContentLoaded", function(event){
     })
     .then(function(result){
         console.log(result);
-        const popularBoxes = document.querySelector(".populor-boxes")
-        console.log(popularBoxes);
+        const popularBoxes = document.querySelector(".popular-boxes")
+        console.log(popularBoxes);                                  
         let popularItems = []
         const popularProducts = result.products
-        for(let index = 0; popularProducts.length; index+=1){
+        let productUsingRender = ""
+        for(let index = 0; index < popularProducts.length; index+=1){
             let product = popularProducts[index]
-            console.log(product);
+            if(product.category == "popular"){
+                popularItems.push(product)
+                let html = `
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="watch-box">
+                            <img src="${product.img_url}"
+                                alt="">
+                            <div class="watch-box-desc">
+                                <h3>Thermo Ball Etip Gloves</h3>
+                                <p>$45,743</p>
+                            </div>
+                        </div>
+                    </div>
+                `
+                productUsingRender +=html
+            }
+            if (popularBoxes) {
+        popularBoxes.innerHTML = productUsingRender;
+            }
         }
+        console.log(popularItems);
+        console.log(product.img_url)
+        parent.innerHTML = productUsingRender
+
         
     })
     .catch(function(error){
